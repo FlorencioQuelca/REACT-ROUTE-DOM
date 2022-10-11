9793a6d45289e60ff61fcc859166df65b764fd9b
FlorencioQuelca <florens.lonwe@gmail.com>
2022-10-10 21:28:29 -0400

    avances

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
+import AboutUs from './components/AboutUs'
+import Proyects from './components/Proyects'
+import Home from './components/Home'
+import Team from './components/Team'
+import Error404 from './components/Error404'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
+      <Header />
       <Routes>
-        <Route  path='/' element={<h1>HOME</h1>} />
+        <Route  path='/' element={<Home/>} />
+        <Route  path='/About' element={<AboutUs/>} />
+        <Route  path='/Proyects' element={<Proyects/>} />
+        <Route  path='/Proyects/Team' element={<Team/>} />
+        <Route  path='*' element={<Error404/>} />
       </Routes>
+      <Footer/>
     </div>
   )
 }
